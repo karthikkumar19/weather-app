6438cd7fb99f19e36563a922b3489862afc07cfb
Karthik kumar <mkarthikkumar19@gmail.com>
2019-11-25 12:04:16 +0530

    axios is installed and response is received

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'weather-icons/css/weather-icons.css';
 import Weather from './app_component/weather_component';
@@ -8,11 +9,29 @@ import './App.css';
 const API_key ="e890e5f43e3114ad60f6c64ccacbd4e3";
 
 class App extends React.Component{
-  state={}
+  state={
+    posts:[],temp:[],desc:[]
+  }
+
+  componentDidMount(){
+    axios.get(
+      'http://api.openweathermap.org/data/2.5/weather?q=London&?units=metric&APPID=e890e5f43e3114ad60f6c64ccacbd4e3'
+      )
+    .then(response => {
+      const posts = response.data;
+      const temp = response.data.main;
+      const desc =  response.data.weather[0];
+      console.log(response.data);
+      this.setState({posts:posts, temp: temp, desc: desc});
+    });
+
+  }
   render(){
     return(
 <div className="App">
-     <Weather/>
+     <Weather name={this.state.posts.name} temp={this.state.temp.temp}
+      min={this.state.temp.temp_min} max={this.state.temp.temp_max}
+      desc={this.state.desc.main} />
     </div>
 
     );
--- app_component/weather_component.js
+++ app_component/weather_component.js
@@ -1,33 +1,28 @@
 import React from 'react';
 
-const weather =() =>{
+const weather =(props) =>{
     return(
         <div className="container">
             <div className="cards">
-            <h1>London</h1>
+            <h1>{props.name}</h1>
             <h5 className="py-4">
             <i class="wi wi-day-sunny display-1"></i>
             </h5>
             <h1 className="py-2">
-                25&deg;
+                {props.temp}&deg;
             </h1>
             {/* {show max and min temp} */}
-            {minmaxTemp(24,19)}
-
-            <h4 className="py-3">slow rain</h4>
+            <h3>
+                <span className="px-4">{props.min}&deg;</span>
+                <span className="px-4">{props.max}&deg;</span>
+            </h3>
+            <h4 className="py-3">{props.desc}</h4>
             </div>
         </div>
     )
 
 
-    function minmaxTemp(min,max){
-        return(
-            <h3>
-                <span className="px-4">{min}&deg;</span>
-                <span className="px-4">{max}&deg;</span>
-            </h3>
-        )
-    }
+   
 }
 
 export default weather;
