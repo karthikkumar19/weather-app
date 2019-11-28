944cd5dd7bce403bea65b35459076a5da0642efd
Karthik kumar <mkarthikkumar19@gmail.com>
2019-11-28 18:00:41 +0530

    country is removed

--- app_component/form_container/form.component.js
+++ app_component/form_container/form.component.js
@@ -25,12 +25,10 @@ handleChange = (e) => {
             <div className="container">
                 <form>
                 <div className="row">
-                    <div className="col-md-3 offset-md-2">
+                    <div className="col-md-3 offset-md-4">
                         <input type="text" className="form-control" name="city" autoComplete="off" placeholder="City" onChange={this.handleChange}/>
                     </div>
-                    <div className="col-md-3">
-                        <input type="text" className="form-control" name="country" autoComplete="off" placeholder="Country" />
-                    </div>
+                   
                     <div className="col-md-3 mt-md-0 py-2 text-md-left">
                     <button className="btn btn-warning" onClick={this.handleLangChange}>Get Weather</button>
                     </div>
